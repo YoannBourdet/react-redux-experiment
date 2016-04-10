3baacb9dbe8745a89ee905fa04035f2f7f46099f
Yoann Bourdet <yoannbourdet@gmail.com>
2016-04-10 18:05:37 +0200

    add module req with fetch api and toastr for notifications

--- app/js/containers/app.jsx
+++ app/js/containers/app.jsx
@@ -1,32 +1,88 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import AutoComplete from 'material-ui/lib/auto-complete';
 
 export default class App extends Component {
-
   render() {
     const fruit = [
-      'Apple', 'Apricot', 'Avocado',
-      'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry',
-      'Boysenberry', 'Blood Orange',
-      'Cantaloupe', 'Currant', 'Cherry', 'Cherimoya', 'Cloudberry',
-      'Coconut', 'Cranberry', 'Clementine',
-      'Damson', 'Date', 'Dragonfruit', 'Durian',
+      'Apple',
+      'Apricot',
+      'Avocado',
+      'Banana',
+      'Bilberry',
+      'Blackberry',
+      'Blackcurrant',
+      'Blueberry',
+      'Boysenberry',
+      'Blood Orange',
+      'Cantaloupe',
+      'Currant',
+      'Cherry',
+      'Cherimoya',
+      'Cloudberry',
+      'Coconut',
+      'Cranberry',
+      'Clementine',
+      'Damson',
+      'Date',
+      'Dragonfruit',
+      'Durian',
       'Elderberry',
-      'Feijoa', 'Fig',
-      'Goji berry', 'Gooseberry', 'Grape', 'Grapefruit', 'Guava',
-      'Honeydew', 'Huckleberry',
-      'Jabouticaba', 'Jackfruit', 'Jambul', 'Jujube', 'Juniper berry',
-      'Kiwi fruit', 'Kumquat',
-      'Lemon', 'Lime', 'Loquat', 'Lychee',
+      'Feijoa',
+      'Fig',
+      'Goji berry',
+      'Gooseberry',
+      'Grape',
+      'Grapefruit',
+      'Guava',
+      'Honeydew',
+      'Huckleberry',
+      'Jabouticaba',
+      'Jackfruit',
+      'Jambul',
+      'Jujube',
+      'Juniper berry',
+      'Kiwi fruit',
+      'Kumquat',
+      'Lemon',
+      'Lime',
+      'Loquat',
+      'Lychee',
       'Nectarine',
-      'Mango', 'Marion berry', 'Melon', 'Miracle fruit', 'Mulberry', 'Mandarine',
-      'Olive', 'Orange',
-      'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Physalis', 'Plum', 'Pineapple',
-      'Pumpkin', 'Pomegranate', 'Pomelo', 'Purple Mangosteen',
+      'Mango',
+      'Marion berry',
+      'Melon',
+      'Miracle fruit',
+      'Mulberry',
+      'Mandarine',
+      'Olive',
+      'Orange',
+      'Papaya',
+      'Passionfruit',
+      'Peach',
+      'Pear',
+      'Persimmon',
+      'Physalis',
+      'Plum',
+      'Pineapple',
+      'Pumpkin',
+      'Pomegranate',
+      'Pomelo',
+      'Purple Mangosteen',
       'Quince',
-      'Raspberry', 'Raisin', 'Rambutan', 'Redcurrant',
-      'Salal berry', 'Satsuma', 'Star fruit', 'Strawberry', 'Squash', 'Salmonberry',
-      'Tamarillo', 'Tamarind', 'Tomato', 'Tangerine',
+      'Raspberry',
+      'Raisin',
+      'Rambutan',
+      'Redcurrant',
+      'Salal berry',
+      'Satsuma',
+      'Star fruit',
+      'Strawberry',
+      'Squash',
+      'Salmonberry',
+      'Tamarillo',
+      'Tamarind',
+      'Tomato',
+      'Tangerine',
       'Ugli fruit',
       'Watermelon',
     ];
@@ -43,19 +99,9 @@ export default class App extends Component {
     ];
     return (
       <div>
-        <AutoComplete
-          filter={AutoComplete.fuzzyFilter}
-          floatingLabelText="Type t, fuzzy search"
-          fullWidth
-          dataSource={fruit}
-        />
+        <AutoComplete filter={AutoComplete.fuzzyFilter} floatingLabelText="Type t, fuzzy search" fullWidth dataSource={fruit}/>
         <br/>
-        <AutoComplete
-          filter={AutoComplete.caseInsensitiveFilter}
-          floatingLabelText="Type r, case insensitive"
-          fullWidth
-          dataSource={colors}
-        />
+        <AutoComplete filter={AutoComplete.caseInsensitiveFilter} floatingLabelText="Type r, case insensitive" fullWidth dataSource={colors}/>
       </div>
     );
   }
