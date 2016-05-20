3fcc2b52bdf7fbbcb06829fb86cd98ada3c99e48
Yoann Bourdet <yoannbourdet@gmail.com>
2016-05-20 23:14:35 +0200

    update pagination && add font awesome

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -70,6 +70,12 @@ module.exports = {
     }, {
       test: /\.css$/,
       loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
+    }, {
+      test: /\.((woff2?|svg)(\?v=[0-9]\.[0-9]\.[0-9]))|(woff2?|svg|jpe?g|png|gif|ico)$/,
+      loader: 'url?limit=10000',
+    }, {
+      test: /\.((ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9]))|(ttf|eot)$/,
+      loader: 'file',
     }],
   },
   plugins: onProduction ? pluginsForDev.push(...pluginsForProd) : pluginsForDev,
